9c767ae2e29f5b2f0c5e6f92d5f9114d9c38b0e5
Abhimanyu Wadhwa <63635609+AbhimanyuW@users.noreply.github.com>
2023-08-16 13:27:13 +0100

    Update split.py

--- split.py
+++ split.py
@@ -4,7 +4,7 @@ import splitfolders
 input_folder = '/Users/abhimanyuwadhwa/Desktop/QMUL/MScProject/DataSet/nsynth-train-all'
 output_folder = '/Users/abhimanyuwadhwa/Desktop/QMUL/MScProject/DataSet/nsynth-split'
 
-split_ratios = (0.7, 0.2, 0.1)  # Train, test, validate
+split_ratios = (0.7, 0.2, 0.1)  # Train, validate, test
 
 # Use the split-folders library to perform the split
 splitfolders.ratio(input_folder, output_folder, seed=42, ratio=split_ratios, group_prefix=None)
